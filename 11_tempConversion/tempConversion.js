04228c5ef03feb91cace96cf736fcdc94a5c9c23
kdthona <38767649+Kdthona@users.noreply.github.com>
2025-12-01 21:33:52 -0800

    added temp conversion functions for celsius and fahrenheit and passed all tests

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const roundFirstDecimal = function(num){
+return Math.round(num * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(fahrenheit) {
+return roundFirstDecimal((fahrenheit - 32) * 5 / 9);
+};
+
+const convertToFahrenheit = function(celsius) {
+return roundFirstDecimal((celsius * 9 / 5) + 32);
 };
 
 // Do not edit below this line
