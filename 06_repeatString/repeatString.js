3ce620c19f2b6067e8e7c390438b9c198cd080dd
kdthona <38767649+Kdthona@users.noreply.github.com>
2025-12-01 20:25:57 -0800

    added function to remove from array

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+	let strSum = '';
+	if(num < 0){
+		return 'ERROR';
+	}else
 
+	for(let i = 0; i < num; i++){
+		strSum += str;
+	}
+
+	return strSum;
 };
 
 // Do not edit below this line
