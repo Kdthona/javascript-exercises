e35c9f9de26b84112ad52f6cccbdf856080cd388
kdthona <38767649+Kdthona@users.noreply.github.com>
2025-12-01 21:19:12 -0800

    added leapYear function and passed all tests

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+const fHundred = (yr) => !(year % 400);
+let oHundred = (yr) => !(year % 100);
+let four = (yr) => !(year % 4);
+
+if(fHundred(year)){
+  return true;
+  }else if (four(year) && !oHundred(year)) {
+  return true;
+  }else return false;
 };
 
 // Do not edit below this line
