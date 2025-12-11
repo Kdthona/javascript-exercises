5a799c59d1185e7312fed2eac9394029d4cc8db2
kdthona <38767649+Kdthona@users.noreply.github.com>
2025-12-10 22:22:04 -0800

    welele

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+return arr.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+return arr.reduce((prod, num) => prod * num, 1);
 };
 
-const power = function() {
-	
+const power = function(b,e) {
+let prod = 1;
+for(let i = 0; i < e; i++){
+prod *= b;
+	}return prod;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+if(!num){return 1;}
+let prod = num;
+for(let i = 1; i < num; i++){
+prod *= num-i;
+	}return prod;
+}
 
 // Do not edit below this line
 module.exports = {
