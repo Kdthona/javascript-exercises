43964d26e0d7766b3dba20cc7d391b7c7c283eb6
kdthona <38767649+Kdthona@users.noreply.github.com>
2025-12-11 18:04:54 -0800

    completed function and passed tests

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
 
+const palindromes = (string) => {
+	const alphanumerical = 'abcdefhijklmnopqrstuvwxyz1234567890';
+
+
+	arr = string
+		.toLowerCase()
+		.split('')
+		.filter((char) => alphanumerical.includes(char))
+		.join('');
+
+	rvrs = arr.split('').reverse().join('');
+
+	return arr == rvrs;
 };
 
 // Do not edit below this line
