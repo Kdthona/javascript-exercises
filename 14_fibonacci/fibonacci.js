b68f4cda236c90acc19b38790d9153fd8fcbf01e
kdthona <38767649+Kdthona@users.noreply.github.com>
2025-12-11 18:04:05 -0800

    finished function and passed test

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+  if(+index < 0){
+	return "OOPS";
+  }else switch(+index){
+	case 0:
+		return 0;
+		break;
+	case 1:
+	case 2:
+		return 1;
+		break;
+	default:
+	  let a = b = 1;
+	  let temp;
+	  for(let i = 2; i < index; i ++){
+		temp = a;
+		a = b;
+		b = a + temp;
+	  } return b;
+  }
 };
 
 // Do not edit below this line
