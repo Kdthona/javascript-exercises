8a420ae28ba924250875ec199ab8789c603d8749
kdthona <38767649+Kdthona@users.noreply.github.com>
2025-12-11 19:18:33 -0800

    completed function and passed all tests

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(pplArr) {
+	console.log("NEW TEST");
+	return pplArr.reduce(function(oldest, person){
+		if(Object.hasOwn(person, "yearOfDeath")){
+			console.log(person.name + " " + (person.yearOfDeath - person.yearOfBirth));
+			if(person.yearOfDeath - person.yearOfBirth > oldest.age){
+				oldest.name = person.name;
+				oldest.age = person.yearOfDeath - person.yearOfBirth;
+			}
+		}else{
+		  if((new Date()).getFullYear() - person.yearOfBirth > oldest.yearOfDeath - oldest.yearOfBirth){
+				oldest.name = person.name;
+				oldest.age = (new Date()).getFullYear() - person.yearOfBirth;
+		  }
+		}return oldest;
+	  }, {
+	  name: '',
+	  yearOfBirth: 0,
+	  yearOfDeath: 0,
+	  age: 0
+	});
 };
 
 // Do not edit below this line
